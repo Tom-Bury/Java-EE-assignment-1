309b1a3ce51393f12125e19ba203c73461909e3b
Stijn <stijn.caluwaerts@gmail.com>
2018-10-16 11:40:14 +0200

    CreateQuote

--- CarRental-ejb/src/java/session/ .java
+++ CarRental-ejb/src/java/session/ .java
@@ -4,13 +4,21 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.ejb.Stateful;
 import rental.RentalStore;
+import rental.ReservationConstraints;
+import rental.Quote;
 
 @Stateful
 public class CarRentalSession implements CarRentalSessionRemote {
+    String name;
+    
 
     @Override
     public Set<String> getAllRentalCompanies() {
         return new HashSet<String>(RentalStore.getRentals().keySet());
+    }
+    
+    public Quote createQuote(ReservationConstraints constraint) {
+        
     }
 
     
