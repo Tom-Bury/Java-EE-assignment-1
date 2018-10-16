c66b0da69041769168a1e91255728767907e62da
Tom Bury <tom.bury@hotmail.com>
2018-10-16 12:32:39 +0200

    Interface update

--- CarRental-lib/src/session/CarRentalSessionRemote.java
+++ CarRental-lib/src/session/CarRentalSessionRemote.java
@@ -1,11 +1,19 @@
 package session;
 
+import java.util.List;
 import java.util.Set;
 import javax.ejb.Remote;
+import rental.Quote;
+import rental.ReservationConstraints;
 
 @Remote
 public interface CarRentalSessionRemote {
 
     Set<String> getAllRentalCompanies();
     
+    List<Quote> createQuote(ReservationConstraints constraint);
+    
+    List<Quote> getCurrentQuotes();
+    
+    void setName(String name);
 }
